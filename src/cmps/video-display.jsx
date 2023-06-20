58791744abc47d4e6f51e82a5cbde5b423f4dca5
ofirezracode <ofirezracode@gmail.com>
2023-06-20 16:00:40 +0300

    UPDATE: currently playing header

--- src/cmps/video-display.jsx
+++ src/cmps/video-display.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
-export function VideoDisplay(props) {
-  const currVideoToDisplay = props.currVideo ? props.currVideo : "W6NZfCO5SIk";
+export function VideoDisplay({ currVideo }) {
+  const videoId = currVideo ? currVideo.videoId : "W6NZfCO5SIk";
+  const videoTitle = currVideo ? currVideo.title : "Introduction to JavaScript";
   return (
     <div className="video-display">
       <iframe
         title="video"
         width="100%"
         height="100%"
-        src={`https://www.youtube.com/embed/${currVideoToDisplay}`}
+        src={`https://www.youtube.com/embed/${videoId}`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
+      <h2>Currently Playing: {videoTitle}</h2>
     </div>
   );
 }
--- src/views/playlist-index.jsx
+++ src/views/playlist-index.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from "react"
-import { SearchBar } from "../cmps/search-bar"
-import { VideosList } from "../cmps/videos-list"
-import { VideoDisplay } from "../cmps/video-display"
-import { videoService } from "../services/video.service"
-import { httpService } from "../services/http.service"
+import React, { useState } from "react";
+import { SearchBar } from "../cmps/search-bar";
+import { VideosList } from "../cmps/videos-list";
+import { VideoDisplay } from "../cmps/video-display";
+import { videoService } from "../services/video.service";
+import { httpService } from "../services/http.service";
 
 export function PlaylistIndex(props) {
-  const [searchTerm, setSearchTerm] = useState("")
-  const [videos, setVideos] = useState([])
-  const [currVideo, setCurrVideo] = useState('')
+  const [searchTerm, setSearchTerm] = useState("");
+  const [videos, setVideos] = useState([]);
+  const [currVideo, setCurrVideo] = useState("");
 
-  console.log('videos - from index: ', videos)
+  console.log("videos - from index: ", videos);
 
   function searchVideo(e) {
-    e.preventDefault()
-    loadVideos()
+    e.preventDefault();
+    loadVideos();
   }
 
   async function loadVideos() {
-    const requestUrl = videoService.getRequestUrl(searchTerm)
-    const videos = await videoService.queryVideos(requestUrl) // returns array of video objects
-    setVideos(videos)
+    const requestUrl = videoService.getRequestUrl(searchTerm);
+    const videos = await videoService.queryVideos(requestUrl); // returns array of video objects
+    setVideos(videos);
   }
 
   return (
@@ -28,11 +28,10 @@ export function PlaylistIndex(props) {
       <SearchBar
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm}
-        searchVideo={searchVideo} />
-      <VideosList
-        videos={videos}
-        setCurrVideo={setCurrVideo} />
-      <VideoDisplay currVideo={currVideo.videoId} />
+        searchVideo={searchVideo}
+      />
+      <VideosList videos={videos} setCurrVideo={setCurrVideo} />
+      <VideoDisplay currVideo={currVideo} />
     </div>
-  )
+  );
 }
